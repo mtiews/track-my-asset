204168f7bd66a34877940b4ae423406b0f644bd1
Marcel Tiews <marcel.tiews@gmail.com>
2017-10-03 12:03:22 +0200

    Bugfix login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,10 +8,14 @@ import { AuthService } from '../shared/services/auth.service';
 })
 export class LoginComponent implements OnInit {
 
+  isAuthenticated = false;
+
   constructor(public auth: AuthService) { }
 
   ngOnInit() {
-    
+    this.auth.isAuthenticated$.subscribe((authenticated) => {
+      this.isAuthenticated = authenticated;
+    });
   }
 
   onLogin() {
